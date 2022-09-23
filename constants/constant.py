b06bc18f446dff7863d06176dbf4de1aa2a03191
devansh-zymr <devansh.v@zymr.com>
2022-09-23 15:33:06 +0530

    changed the location of alembic file

---
+++ constants/constant.py
@@ -0,0 +1,6 @@
+import enum
+import pathlib
+
+
+# class Path:
+ALEMBIC_PATH = str(pathlib.Path(__file__).parent.parent.absolute()) + "/database/alembic.ini"
--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 from flask import Flask
 import os
 from database import db_models
+import pathlib
+from constants.constant import ALEMBIC_PATH
+
+absolute_path = str(pathlib.Path(__file__).parent.absolute()) + "/database/alembic.ini"
 
 app = Flask(__name__)
 db_models.create_model()
 
 if __name__ == "__main__":
-    os.system("alembic upgrade head")  # upgrade database to latest version
+    os.system("pwd")
+    os.system(f"alembic -c {ALEMBIC_PATH} upgrade head")  # upgrade database to latest version
     print("started..............")
     app.run(host="0.0.0.0", port=8000, debug=True)
