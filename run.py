f865e4a3e0230ce7389e628c3f0f16fb8df998c1
pranayprajapati-zymr <pranay.prajapati@zymr.com>
2022-09-26 17:50:50 +0530

    alembic fixes

--- run.py
+++ run.py
@@ -1,15 +1,11 @@
 from flask import Flask
 import os
 from database import db_models
-import pathlib
-
-absolute_path = str(pathlib.Path(__file__).parent.absolute()) + "/database/alembic.ini"
 
 app = Flask(__name__)
 db_models.create_model()
 
 if __name__ == "__main__":
-
-    os.system(f"alembic -c {absolute_path} upgrade head")  # upgrade database to latest version
+    os.system(f"alembic upgrade head")  # upgrade database to latest version
     print("started..............")
     app.run(host="0.0.0.0", port=8000, debug=True)
