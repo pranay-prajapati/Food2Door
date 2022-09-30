6a1bae3dd1c69648d87100ac2975c785fb795c96
pranayprajapati-zymr <pranay.prajapati@zymr.com>
2022-09-30 17:02:27 +0530

    signup api optimization and added setting to disable csrf token in form

--- service/__init__.py
+++ service/__init__.py
@@ -1,12 +1,12 @@
+import os
 from flask import Flask
 from service.user.routes import user_management_route
 
 
 def create_app():
-
     app = Flask(__name__)
+    app.config["WTF_CSRF_ENABLED"] = False
 
-    import os
     secret_key = os.urandom(32)
     app.config['SECRET_KEY'] = secret_key
 
