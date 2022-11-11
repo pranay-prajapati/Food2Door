c70f39f7f840bf9381db39a395bc984037d160e1
pranayprajapati-zymr <pranay.prajapati@zymr.com>
2022-11-11 14:51:46 +0530

    FDR-0012/rabbitmq-setup: initial

---
+++ common/rabbitmq_config.py
@@ -0,0 +1,17 @@
+from enum import Enum
+from os import getenv
+
+
+class Queues(Enum):
+    EMAIL_NOTIFICATION = "email_notification"
+    ORDER_QUEUE = "order_queue"
+
+
+class RabbitmqConfig:
+    host = getenv("RABBITMQ_HOST", "localhost")
+    port = int(getenv("RABBITMQ_PORT", "5672"))
+    attempts = 10
+    retry_delay_sec = int(getenv("RABBITMQ_DELAY_RETRY_DELAY_SEC", "5"))
+    ttl = int(getenv("RABBITMQ_TTL_SEC", "10000"))
+    retry_count = int(getenv("RETRY_MAX_RETRY_COUNT", "3"))
+    exchange = getenv("RABBITMQ_DEFAULT_EXCHANGE", 'amq.direct')
---
+++ common/rabbitmq_service.py
@@ -0,0 +1,16 @@
+class RabbitMQService:
+
+    def create_queue(self):
+        pass
+
+    def send_msg_to_queue(self):
+        pass
+
+    def create_retry_queue(self):
+        pass
+
+    def send_email_notification(self):
+        pass
+
+    def publish_message(self):
+        pass
